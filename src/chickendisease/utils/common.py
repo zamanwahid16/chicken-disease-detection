5b6c7dc5f9119c22e078970199a6b602d4fe4fcb
Zaman Wahid <zamanwahid16@gmail.com>
2023-12-21 17:37:51 -0700

    add common utility module

---
+++ src/chickendisease/utils/common.py
@@ -0,0 +1,151 @@
+# module for common utility functions
+
+import os
+import yaml
+import sys
+import json
+from chickendisease import logger
+from box.exceptions import BoxValueError
+import joblib
+from pathlib import Path
+from typing import Any
+import base64
+
+
+@ensure_annotations
+def read_yaml(path: Path) -> ConfigBox:
+    """
+    Read YAML file and return a Box object.
+    
+    :param path: Path to YAML file.
+    :return: Box object.
+    """
+
+    try:
+        with open(path, "r") as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"Loaded YAML file from {path}")
+            return Box(content)
+    except BoxValueError as e:
+        raise BoxValueError(f"Error reading YAML file from {path}")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict) -> None:
+    """
+    Save data as JSON file.
+    
+    :param path: Path to save JSON file.
+    :param data: Data to save.
+    """
+
+    try:
+        with open(path, "w") as json_file:
+            json.dump(data, json_file, indent=4)
+            logger.info(f"Saved JSON file to {path}")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """
+    Load JSON file.
+    
+    :param path: Path to JSON file.
+    :return: Loaded JSON file as ConfigBox class.
+    """
+
+    try:
+        with open(path, "r") as json_file:
+            data = json.load(json_file)
+            logger.info(f"Loaded JSON file from {path}")
+            return ConfigBox(data)
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def save_binary(path: Path, data: Any) -> None:
+    """
+    Save data as binary file.
+    
+    :param path: Path to save binary file.
+    :param data: Data to save.
+    """
+
+    try:
+        with open(path, "wb") as binary_file:
+            joblib.dump(data, binary_file)
+            logger.info(f"Saved binary file to {path}")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def load_binary(path: Path) -> Any:
+    """
+    Load binary file.
+    
+    :param path: Path to binary file.
+    :return: Loaded binary file.
+    """
+
+    try:
+        data = joblib.load(binary_file)
+        logger.info(f"Loaded binary file from {path}")
+        return data
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """
+    Get file size in KB.
+    
+    :param path: Path to file.
+    :return: File size in KB.
+    """
+
+    try:
+        size = round(os.path.getsize(path) / 1000)
+        return f"~ {size} KB"
+    except Exception as e:
+        raise e
+
+
+def decode_image(img_string: str, file_name: str) -> None:
+    """
+    Decode image string and save as image file.
+    
+    :param img_string: Image string.
+    :param file_name: Name of image file.
+    """
+
+    try:
+        img_data = base64.b64decode(img_string)
+        with open(file_name, "wb") as f:
+            f.write(img_data)
+            f.close()
+    except Exception as e:
+        raise e
+
+
+def encode_image(file_name: str) -> str:
+    """
+    Encode image file as string.
+    
+    :param file_name: Name of image file.
+    :return: Image string.
+    """
+
+    try:
+        with open(file_name, "rb") as f:
+            img_string = base64.b64encode(f.read())
+            f.close()
+            return img_string
+    except Exception as e:
+        raise e
