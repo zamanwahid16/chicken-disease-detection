b5ba362bb2e3dbcb0033a9f7ff4bad93e34b0f78
Zaman Wahid <zamanwahid16@gmail.com>
2023-12-25 23:17:47 -0700

    added configuration entity module

---
+++ src/chickendisease/entity/config_entity.py
@@ -0,0 +1,12 @@
+"""Entity module for config."""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    """Data Ingestion Config Entity Class"""
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
