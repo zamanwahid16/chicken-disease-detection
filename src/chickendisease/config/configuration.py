e664520a9fba9c807b17d6d0f4dc72cc794332ed
Zaman Wahid <zamanwahid16@gmail.com>
2023-12-27 23:40:24 -0700

    base model configuration manager

--- src/chickendisease/config/configuration.py
+++ src/chickendisease/config/configuration.py
@@ -1,7 +1,8 @@
 """Configuration for the project."""
 
 from chickendisease.constants import *
-from chickendisease.entity.config_entity import DataIngestionConfig
+from chickendisease.entity.config_entity import (BaseModelConfig,
+                                                 DataIngestionConfig)
 from chickendisease.utils.common import make_dir, read_yaml
 
 
@@ -28,3 +29,19 @@ class ConfigurationManager:
             local_data_file=config.local_data_file
         )
         return data_ingestion_config
+
+    def get_base_model_config(self) -> BaseModelConfig:
+        """Returns the BaseModelConfig object."""
+        config = self.config.base_model
+        make_dir([config.root_dir])
+        base_model_config = BaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+        return base_model_config
