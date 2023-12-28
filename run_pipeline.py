a472e41ac73a8675840f400885a2baae7317b3e3
Zaman Wahid <zamanwahid16@gmail.com>
2023-12-27 23:46:25 -0700

    added the base model pipeline in run

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,15 +1,29 @@
 """ run the pipeline """
 
 from chickendisease import logger
+from chickendisease.pipeline.base_model import BaseModelPipeline
 from chickendisease.pipeline.data_ingestion import DataIngestionPipeline
 
 # Stage 1: Data Ingestion
 STAGE_NAME = 'Data Ingestion'
 try:
     logger.info(f'>>>>> Running pipeline for {STAGE_NAME} <<<<<')
-    pipeline = DataIngestionPipeline()
-    pipeline.main()
-    logger.info(f'>>>>> Completed pipeline for {STAGE_NAME} <<<<<')
+    data_pipeline = DataIngestionPipeline()
+    data_pipeline.main()
+    logger.info(
+        f'>>>>> Completed pipeline for {STAGE_NAME}<<<<<\n\nx======================x======================x\n\n')
+except Exception as e:
+    logger.error(f'Failed to run the pipeline for {STAGE_NAME} with exception: {e}')
+    raise e
+
+# Stage 2: Base Model
+STAGE_NAME = 'Base Model'
+try:
+    logger.info(f'>>>>> Running pipeline for {STAGE_NAME} <<<<<')
+    model_pipeline = BaseModelPipeline()
+    model_pipeline.main()
+    logger.info(
+        f'>>>>> Completed pipeline for {STAGE_NAME}<<<<<\n\nx======================x======================x\n\n')
 except Exception as e:
     logger.error(f'Failed to run the pipeline for {STAGE_NAME} with exception: {e}')
     raise e
