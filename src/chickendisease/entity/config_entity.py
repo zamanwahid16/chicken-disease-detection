074bf960e3fea963b624cfbb6adbe27f53429dc8
Zaman Wahid <zamanwahid16@gmail.com>
2023-12-27 23:38:44 -0700

    added the base model config entity class

--- src/chickendisease/entity/config_entity.py
+++ src/chickendisease/entity/config_entity.py
@@ -10,3 +10,16 @@ class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
+
+
+@dataclass(frozen=True)
+class BaseModelConfig:
+    """Base Model Config Entity Class"""
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
