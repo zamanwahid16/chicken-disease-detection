ea74ede348075b0158ada181060fe92bf2687d79
Zaman Wahid <zamanwahid16@gmail.com>
2023-12-28 13:29:01 -0700

    update the pipeline with model training

--- run_pipeline.py
+++ run_pipeline.py
@@ -3,6 +3,7 @@
 from chickendisease import logger
 from chickendisease.pipeline.base_model import BaseModelPipeline
 from chickendisease.pipeline.data_ingestion import DataIngestionPipeline
+from chickendisease.pipeline.trainer import ModelTrainer
 
 # Stage 1: Data Ingestion
 STAGE_NAME = 'Data Ingestion'
@@ -16,7 +17,7 @@ except Exception as e:
     logger.error(f'Failed to run the pipeline for {STAGE_NAME} with exception: {e}')
     raise e
 
-# Stage 2: Base Model
+# Stage 2: Prepare Base Model
 STAGE_NAME = 'Base Model'
 try:
     logger.info(f'>>>>> Running pipeline for {STAGE_NAME} <<<<<')
@@ -24,6 +25,18 @@ try:
     model_pipeline.main()
     logger.info(
         f'>>>>> Completed pipeline for {STAGE_NAME}<<<<<\n\nx======================x======================x\n\n')
+except Exception as e:
+    logger.error(f'Failed to run the pipeline for {STAGE_NAME} with exception: {e}')
+    raise e
+
+# Stage 3: Training the Model
+STAGE_NAME = 'Training'
+try:
+    logger.info(f'>>>>> Running pipeline for {STAGE_NAME} <<<<<')
+    trainer = ModelTrainer()
+    trainer.main()
+    logger.info(
+        f'>>>>> Completed pipeline for {STAGE_NAME} <<<<<\n\nx======================x======================x\n\n')
 except Exception as e:
     logger.error(f'Failed to run the pipeline for {STAGE_NAME} with exception: {e}')
     raise e
