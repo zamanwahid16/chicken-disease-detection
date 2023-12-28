ad3aa8db1c3191dcc0d3ab4352aa039c2e685504
Zaman Wahid <zamanwahid16@gmail.com>
2023-12-28 13:24:15 -0700

    added model training config entity

--- src/chickendisease/entity/config_entity.py
+++ src/chickendisease/entity/config_entity.py
@@ -31,3 +31,16 @@ class CallbacksConfig:
     root_dir: Path
     tensorboard_logs_dir: Path
     checkpoint_model_path: Path
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    """Training Config Entity Class"""
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augment: bool
+    params_image_size: list
